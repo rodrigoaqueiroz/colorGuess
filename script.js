b52ca14217172ba1f7bd309ea5701a9cb73f00a1
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-03-13 19:27:47 -0300

    fim dos requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-function sortCorrectColor() {
-  const rgbArray = [
-    Math.ceil(Math.random() * 256),
-    Math.ceil(Math.random() * 256),
-    Math.ceil(Math.random() * 256)];
-  const correctColor = rgbArray.join(', ');
-  const colorFormat = `rgb: ( ${correctColor} )`;
-  document.getElementById('rgb-color').innerText = colorFormat;
-}
-
 function makeColorCircles() {
   const getCircle = document.querySelectorAll('.ball');
   for (let index = 0; index < getCircle.length; index += 1) {
     const colorArray = [
-      Math.ceil(Math.random() * 256),
-      Math.ceil(Math.random() * 256),
-      Math.ceil(Math.random() * 256)];
+      Math.ceil(Math.random() * 257),
+      Math.ceil(Math.random() * 257),
+      Math.ceil(Math.random() * 257)];
     const correctColor = colorArray.join(', ');
     const rgbFormat = `rgb(${correctColor})`;
     getCircle[index].style.backgroundColor = rgbFormat;
   }
 }
 
-sortCorrectColor();
+function sortAnswer() {
+  const randomChoice = Math.ceil(Math.random() * 6) - 1
+  const getCircle = document.querySelectorAll('.ball');
+  getCircle[randomChoice].setAttribute('id', 'correct')
+  let rgbAnswer = window.getComputedStyle(getCircle[randomChoice]).getPropertyValue("background-color")
+  document.getElementById('rgb-color').innerText = rgbAnswer; 
+}   
+
+function testShot() {
+  const getCircle = document.querySelectorAll('.ball')
+  for (let index = 0; index < getCircle.length; index += 1) {
+    getCircle[index].addEventListener('click', function(event) {
+      let shot = window.getComputedStyle(event.target).getPropertyValue("background-color");
+      if (shot === document.getElementById('rgb-color').innerText) {
+        document.getElementById('answer').innerText = 'Acertou!'; 
+      } else {
+        document.getElementById('answer').innerText = 'Errou! Tente novamente!!'; 
+      }
+    })
+  }
+}
+
+function resetGame() {
+  let getBtn = document.getElementById('reset-game');
+  getBtn.addEventListener('click', () => {
+    window.location.reload()
+  })
+}
+
 makeColorCircles();
+sortAnswer()
+testShot() 
+resetGame()
